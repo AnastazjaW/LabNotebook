74a2d9c2aceea456d5f1958f211aebba036c85e6
AnastazjaW <twj email>
2022-11-28 20:39:37 +0100

    Initial commit

---
+++ src/main/java/pl/coderslab/LabNotebook/LabNotebookApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.LabNotebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabNotebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabNotebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/coderslab/LabNotebook/LabNotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.LabNotebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabNotebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
